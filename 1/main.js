b567d422b2073542d627c2bc2775da1656c1b67e
Yash1434 <tibadiyayash@gmail.com>
2024-01-16 08:36:09 +0530

    learning

---
+++ 1/main.js
@@ -0,0 +1,10 @@
+// import {a, b, d} from './mymodule.js'
+// console.log(a, b, d)
+
+// import obj from './mymodule.js'
+// console.log(obj)
+
+import yash from './mymodule.js'  // give any name and import (export default from other file)
+console.log(yash)
+
+// https://www.youtube.com/watch?v=bU69doALJGU
